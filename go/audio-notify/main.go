06868e64b316ff8757f5b1f93cbcdf16e3f7a130
clairdl <52753311+clairdl@users.noreply.github.com>
2021-10-12 00:53:08 -0700

    works but Play() skips every other call

--- go/audio-notify/main.go
+++ go/audio-notify/main.go
@@ -12,50 +12,7 @@ import (
 	"github.com/faiface/beep/speaker"
 )
 
-type audioPlayer interface {
-	initPlayer()
-	play()
-}
-
-type audioSource struct {
-	streamer beep.StreamSeekCloser
-	format   beep.Format
-	done     chan bool
-}
-
-func (s audioSource) play() {
-	fmt.Println("play hit")
-	speaker.Play(beep.Seq(s.streamer, beep.Callback(func() {
-		s.done <- true
-	})))
-}
-
-func initPlayer(f *os.File) (*audioSource, error) {
-	streamer, format, err := mp3.Decode(f)
-	if err != nil {
-		return nil, err
-	}
-	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
-
-	avs := audioSource{
-		streamer,
-		format,
-		make(chan bool),
-	}
-
-	return &avs, nil
-}
-
 func main() {
-	f, err := os.Open("notif.mp3")
-	if err != nil {
-		log.Fatal(err)
-	}
-	avs, err := initPlayer(f)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	args := os.Args[1:]
 
 	interval, err := strconv.Atoi(args[0])
@@ -63,21 +20,30 @@ func main() {
 		log.Fatalf("\nERR invalid input: interval argument (%v) is not an integer", args[0])
 	}
 
+	f, err := os.Open("notif.mp3")
+	if err != nil {
+		log.Fatal(err)
+	}
+	streamer, format, err := mp3.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// defer streamer.Close()
+
 	ticker := time.NewTicker(time.Second * time.Duration(interval))
-	defer ticker.Stop()
+	// defer ticker.Stop()
 
-	speaker.Init(avs.format.SampleRate, avs.format.SampleRate.N(time.Second/10))
-
-	ctrl := &beep.Ctrl{Streamer: beep.Loop(-1, avs.streamer), Paused: false}
-	speaker.Play(ctrl)
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
 
 	go func() {
 		for {
 			select {
 			case <-ticker.C:
-				fmt.Println("play invoked")
-				speaker.Clear()
-				speaker.Play(beep.Seq(avs.streamer))
+				fmt.Println("play")
+
+				speaker.Play(beep.Seq(streamer), beep.Callback(func() {
+					streamer.Seek(0)
+				}))
 			}
 
 		}
